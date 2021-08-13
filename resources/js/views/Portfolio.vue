a826e3259724814b9928ae9691b13117b8425ef4
umaporn_do <umaporn_do@dtgo.com>
2021-08-13 09:21:26 +0700

    Changed theme color.

--- resources/js/views/Portfolio.vue
+++ resources/js/views/Portfolio.vue
@@ -6,7 +6,7 @@
 				<div class="row g-0">
 					<div class="col-lg-4 col-sm-6">
 						<a class="portfolio-box" href="images/portfolio/fullsize/1.jpg" title="Project Name">
-							<img class="img-fluid" src="images/portfolio/thumbnails/1.jpg" alt="..." />
+							<img class="img-fluid" src="images/portfolio/thumbnails/1.jpg" alt="..."/>
 							<div class="portfolio-box-caption">
 								<div class="project-category text-white-50">Category</div>
 								<div class="project-name">Project Name</div>
@@ -15,16 +15,16 @@
 					</div>
 					<div class="col-lg-4 col-sm-6">
 						<a class="portfolio-box" href="images/portfolio/fullsize/2.jpg" title="Project Name">
-							<img class="img-fluid" src="images/portfolio/thumbnails/2.jpg" alt="..." />
+							<img class="img-fluid" src="images/portfolio/thumbnails/2.jpg" alt="..."/>
 							<div class="portfolio-box-caption">
 								<div class="project-category text-white-50">Category</div>
 								<div class="project-name">Project Name</div>
 							</div>
-						</a>
+						</a>s
 					</div>
 					<div class="col-lg-4 col-sm-6">
 						<a class="portfolio-box" href="images/portfolio/fullsize/3.jpg" title="Project Name">
-							<img class="img-fluid" src="images/portfolio/thumbnails/3.jpg" alt="..." />
+							<img class="img-fluid" src="images/portfolio/thumbnails/3.jpg" alt="..."/>
 							<div class="portfolio-box-caption">
 								<div class="project-category text-white-50">Category</div>
 								<div class="project-name">Project Name</div>
@@ -33,7 +33,7 @@
 					</div>
 					<div class="col-lg-4 col-sm-6">
 						<a class="portfolio-box" href="images/portfolio/fullsize/4.jpg" title="Project Name">
-							<img class="img-fluid" src="images/portfolio/thumbnails/4.jpg" alt="..." />
+							<img class="img-fluid" src="images/portfolio/thumbnails/4.jpg" alt="..."/>
 							<div class="portfolio-box-caption">
 								<div class="project-category text-white-50">Category</div>
 								<div class="project-name">Project Name</div>
@@ -42,7 +42,7 @@
 					</div>
 					<div class="col-lg-4 col-sm-6">
 						<a class="portfolio-box" href="images/portfolio/fullsize/5.jpg" title="Project Name">
-							<img class="img-fluid" src="images/portfolio/thumbnails/5.jpg" alt="..." />
+							<img class="img-fluid" src="images/portfolio/thumbnails/5.jpg" alt="..."/>
 							<div class="portfolio-box-caption">
 								<div class="project-category text-white-50">Category</div>
 								<div class="project-name">Project Name</div>
@@ -51,7 +51,7 @@
 					</div>
 					<div class="col-lg-4 col-sm-6">
 						<a class="portfolio-box" href="images/portfolio/fullsize/6.jpg" title="Project Name">
-							<img class="img-fluid" src="images/portfolio/thumbnails/6.jpg" alt="..." />
+							<img class="img-fluid" src="images/portfolio/thumbnails/6.jpg" alt="..."/>
 							<div class="portfolio-box-caption p-3">
 								<div class="project-category text-white-50">Category</div>
 								<div class="project-name">Project Name</div>
@@ -65,6 +65,5 @@
 </template>
 
 <script>
-
 
 </script>
