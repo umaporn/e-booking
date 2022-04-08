a6bc69790a4c3940270e2205aa7c14e5801de575
Amnuayporn Jankham <amnuayporn_ja@dtgo.com>
2022-04-08 17:33:34 +0700

    edit function images promotion page

--- resources/views/promotion.blade.php
+++ resources/views/promotion.blade.php
@@ -8,12 +8,16 @@
                 <div class="row uk-margin-medium-top">
                     <div class="col-12 col-md-6 col-xl-4">
                         <article class="uk-article uk-margin-large-bottom">
-                            <div class="uk-inline">
+                            <figure class="online-content image-16by9 position-relative ">
                                 <img class="img-fluid" src="images/promotion/Whizdom-Destinia-The-Forestias-1@2x.png">
                                 <div class="uk-position-top-right promotion-tag-top">NEW</div>
-                                <div class="uk-position-bottom-left promotion-tag-bottom-date article-title">30</div>
-                                <div class="uk-position-bottom-left promotion-tag-bottom-month-year">JUL 2021</div>
-                            </div>
+                                <div class="uk-text-center uk-position-bottom-left promotion-tag-bottom ml-0" uk-grid>
+                                    <div class="uk-width-1-3 pl-0 promotion-tag-bottom-date article-title">30</div>
+                                    <div class="uk-width-2-3 pl-0 promotion-tag-bottom-month-year d-table">
+                                        <span class="d-table-cell align-middle">JUL 2021</span>
+                                    </div>
+                                </div>
+                            </figure>
                             <h2 class="uk-text-truncate article-title uk-margin-small-top uk-margin-small-bottom">
                                 <a class="uk-link-reset" href="http://e-booking.local/promotion/detail">WISDOM ESSENCE
                                                                                                         SUKHUMVIT</a>
@@ -23,12 +27,16 @@
                     </div>
                     <div class="col-12 col-md-6 col-xl-4">
                         <article class="uk-article uk-margin-large-bottom">
-                            <div class="uk-inline">
-                                <img class="img-fluid" src="images/promotion/Whizdom-Destinia-The-Forestias-2@2x.png">
+                            <figure class="online-content image-16by9 position-relative">
+                                <img src="images/promotion/Whizdom-Destinia-The-Forestias-2@2x.png">
                                 <div class="uk-position-top-right promotion-tag-top">NEW</div>
-                                <div class="uk-position-bottom-left promotion-tag-bottom-date article-title">30</div>
-                                <div class="uk-position-bottom-left promotion-tag-bottom-month-year">JUL 2021</div>
-                            </div>
+                                <div class="uk-text-center uk-position-bottom-left promotion-tag-bottom ml-0" uk-grid>
+                                    <div class="uk-width-1-3 pl-0 promotion-tag-bottom-date article-title">30</div>
+                                    <div class="uk-width-2-3 pl-0 promotion-tag-bottom-month-year d-table">
+                                        <span class="d-table-cell align-middle">JUL 2021</span>
+                                    </div>
+                                </div>
+                            </figure>
                             <h2 class="uk-text-truncate article-title uk-margin-small-top uk-margin-small-bottom">
                                 <a class="uk-link-reset" href="http://e-booking.local/promotion/detail">WISDOM ESSENCE
                                                                                                         SUKHUMVIT</a>
@@ -38,12 +46,16 @@
                     </div>
                     <div class="col-12 col-md-6 col-xl-4">
                         <article class="uk-article uk-margin-large-bottom">
-                            <div class="uk-inline">
+                            <figure class="online-content image-16by9 position-relative">
                                 <img class="img-fluid" src="images/promotion/PR-News-Whizdom@2x.png">
                                 <div class="uk-position-top-right promotion-tag-top">NEW</div>
-                                <div class="uk-position-bottom-left promotion-tag-bottom-date article-title">30</div>
-                                <div class="uk-position-bottom-left promotion-tag-bottom-month-year">JUL 2021</div>
-                            </div>
+                                <div class="uk-text-center uk-position-bottom-left promotion-tag-bottom ml-0" uk-grid>
+                                    <div class="uk-width-1-3 pl-0 promotion-tag-bottom-date article-title">30</div>
+                                    <div class="uk-width-2-3 pl-0 promotion-tag-bottom-month-year d-table">
+                                        <span class="d-table-cell align-middle">JUL 2021</span>
+                                    </div>
+                                </div>
+                            </figure>
                             <h2 class="uk-text-truncate article-title uk-margin-small-top uk-margin-small-bottom">
                                 <a class="uk-link-reset" href="http://e-booking.local/promotion/detail">WISDOM ESSENCE
                                                                                                         SUKHUMVIT</a>
@@ -53,12 +65,16 @@
                     </div>
                     <div class="col-12 col-md-6 col-xl-4">
                         <article class="uk-article uk-margin-large-bottom">
-                            <div class="uk-inline">
+                            <figure class="online-content image-16by9 position-relative">
                                 <img class="img-fluid" src="images/promotion/Whizdom-Destinia-The-Forestias-4@2x.png">
                                 <div class="uk-position-top-right promotion-tag-top">NEW</div>
-                                <div class="uk-position-bottom-left promotion-tag-bottom-date article-title">30</div>
-                                <div class="uk-position-bottom-left promotion-tag-bottom-month-year">JUL 2021</div>
-                            </div>
+                                <div class="uk-text-center uk-position-bottom-left promotion-tag-bottom ml-0" uk-grid>
+                                    <div class="uk-width-1-3 pl-0 promotion-tag-bottom-date article-title">30</div>
+                                    <div class="uk-width-2-3 pl-0 promotion-tag-bottom-month-year d-table">
+                                        <span class="d-table-cell align-middle">JUL 2021</span>
+                                    </div>
+                                </div>
+                            </figure>
                             <h2 class="uk-text-truncate article-title uk-margin-small-top uk-margin-small-bottom">
                                 <a class="uk-link-reset" href="http://e-booking.local/promotion/detail">WISDOM ESSENCE
                                                                                                         SUKHUMVIT</a>
@@ -68,12 +84,16 @@
                     </div>
                     <div class="col-12 col-md-6 col-xl-4">
                         <article class="uk-article uk-margin-large-bottom">
-                            <div class="uk-inline">
+                            <figure class="online-content image-16by9 position-relative">
                                 <img class="img-fluid" src="images/promotion/Whizdom Asoke-Sukhumvit-REVIEW-Product@2x.png">
                                 <div class="uk-position-top-right promotion-tag-top">NEW</div>
-                                <div class="uk-position-bottom-left promotion-tag-bottom-date article-title">30</div>
-                                <div class="uk-position-bottom-left promotion-tag-bottom-month-year">JUL 2021</div>
-                            </div>
+                                <div class="uk-text-center uk-position-bottom-left promotion-tag-bottom ml-0" uk-grid>
+                                    <div class="uk-width-1-3 pl-0 promotion-tag-bottom-date article-title">30</div>
+                                    <div class="uk-width-2-3 pl-0 promotion-tag-bottom-month-year d-table">
+                                        <span class="d-table-cell align-middle">JUL 2021</span>
+                                    </div>
+                                </div>
+                            </figure>
                             <h2 class="uk-text-truncate article-title uk-margin-small-top uk-margin-small-bottom">
                                 <a class="uk-link-reset" href="http://e-booking.local/promotion/detail">WISDOM ESSENCE
                                                                                                         SUKHUMVIT</a>
@@ -83,12 +103,16 @@
                     </div>
                     <div class="col-12 col-md-6 col-xl-4">
                         <article class="uk-article uk-margin-large-bottom">
-                            <div class="uk-inline">
+                            <figure class="online-content image-16by9 position-relative">
                                 <img class="img-fluid" src="images/promotion/Whizdom-Destinia-The-Forestias-6@2x.png">
                                 <div class="uk-position-top-right promotion-tag-top">NEW</div>
-                                <div class="uk-position-bottom-left promotion-tag-bottom-date article-title">30</div>
-                                <div class="uk-position-bottom-left promotion-tag-bottom-month-year">JUL 2021</div>
-                            </div>
+                                <div class="uk-text-center uk-position-bottom-left promotion-tag-bottom ml-0" uk-grid>
+                                    <div class="uk-width-1-3 pl-0 promotion-tag-bottom-date article-title">30</div>
+                                    <div class="uk-width-2-3 pl-0 promotion-tag-bottom-month-year d-table">
+                                        <span class="d-table-cell align-middle">JUL 2021</span>
+                                    </div>
+                                </div>
+                            </figure>
                             <h2 class="uk-text-truncate article-title uk-margin-small-top uk-margin-small-bottom">
                                 <a class="uk-link-reset" href="http://e-booking.local/promotion/detail">เช็คอินคาเฟ่ธีมคริสต์มาส
                                                                                                         ที่ WHIZDOM
